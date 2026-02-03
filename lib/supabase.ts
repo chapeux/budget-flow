b82c06722d746ffd524fde96cbcd88c0e076fc8d
Luan Schappo <110139022+chapeux@users.noreply.github.com>
2026-02-03 16:02:04 -0300

    Switch to using import.meta.env for Supabase config

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.SUPABASE_URL || '';
-const supabaseKey = process.env.SUPABASE_KEY || '';
+// Altere de process.env para import.meta.env
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseKey) {
   console.warn('Supabase URL or Key is missing. Please check your environment variables.');
