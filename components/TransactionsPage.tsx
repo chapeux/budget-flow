f5bc4456525b1a12404ecb74d642c05b3cca6976
Luan Schappo <110139022+chapeux@users.noreply.github.com>
2026-02-05 15:08:03 -0300

    change position

--- components/TransactionsPage.tsx
+++ components/TransactionsPage.tsx
@@ -684,8 +684,8 @@ export const TransactionsPage: React.FC<TransactionsPageProps> = ({
 
                 {/* Current */}
                 <div className="relative pl-6 border-l-2 border-blue-500">
-                    <div className="absolute -left-[9px] top-0 w-4 h-4 rounded-full bg-blue-500 border-2 border-white dark:border-slate-900 animate-pulse"></div>
                     <div className="absolute -left-[9px] top-0 w-4 h-4 rounded-full bg-white border-2 border-white dark:border-slate-900"></div>
+                    <div className="absolute -left-[9px] top-0 w-4 h-4 rounded-full bg-blue-500 border-2 border-white dark:border-slate-900 animate-pulse"></div>
                     <p className="text-xs font-semibold text-blue-500 uppercase tracking-wider mb-1">Saldo acumulado (Efetivado)</p>
                     <p className="text-2xl font-bold text-slate-800 dark:text-white">{displayValue(balanceInfo.current)}</p>
                 </div>
